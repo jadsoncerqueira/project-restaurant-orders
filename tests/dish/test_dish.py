fdca0c19c0e48d2ed6726ad925fcea633d6db68b
Jadson Cerqueira <jadsoncerqueira97@gmail.com>
2023-04-24 13:36:01 -0300

    Requisito 1 e 2

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,57 @@
 from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
+import pytest
 
 
 # Req 2
 def test_dish():
-    pass
+    first_dish = Dish("lasanha presunto", 25.90)
+    second_dish = Dish("lasanha berinjela", 27.00)
+    third_dish = Dish("lasanha berinjela", 27.00)
+    first_ingredient = Ingredient("manteiga")
+    second_ingredient = Ingredient("bacon")
+    first_dish.add_ingredient_dependency(first_ingredient, 1)
+    second_dish.add_ingredient_dependency(second_ingredient, 2)
+
+    # a classe pode ser instanciada corretamente
+
+    assert first_dish.name == "lasanha presunto"
+    assert first_dish.price == 25.90
+    assert second_dish.name == "lasanha berinjela"
+    assert second_dish.price == 27.00
+
+    # os métodos da classe, inclusive os métodos mágicos, funcionem
+
+    # __repr__
+    assert repr(first_dish) == "Dish('lasanha presunto', R$25.90)"
+    assert repr(second_dish) == "Dish('lasanha berinjela', R$27.00)"
+
+    # __eq__
+    assert first_dish != second_dish
+    assert second_dish == third_dish
+
+    # __hash__
+    assert hash(first_dish) != hash(second_dish)
+    assert hash(second_dish) == hash(third_dish)
+
+    # add_ingredient_dependency
+    assert first_dish.recipe == {first_ingredient: 1}
+    assert second_dish.recipe == {second_ingredient: 2}
+
+    # get_restrictions
+    assert first_dish.get_restrictions() == {
+        Restriction.ANIMAL_DERIVED,
+        Restriction.LACTOSE,
+    }
+
+    # get_ingredients
+    assert first_dish.get_ingredients() == {first_ingredient}
+    assert second_dish.get_ingredients() == {second_ingredient}
+
+    # são levantados erros ao criar pratos inválidos
+
+    with pytest.raises(ValueError):
+        Dish("lasanha presuto", -5.0)
+
+    with pytest.raises(TypeError):
+        Dish("Lasanha", "50.0")
--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,35 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction
 
 
 # Req 1
 def test_ingredient():
-    pass
+    # o atributo conjunto restrictions é populado como esperado
+
+    first_ingredient = Ingredient("caldo de carne")
+    second_ingredient = Ingredient("bacon")
+    third_ingredient = Ingredient("manteiga")
+    fourth_ingredient = Ingredient("bacon")
+    assert Restriction.ANIMAL_DERIVED in first_ingredient.restrictions
+    assert Restriction.ANIMAL_DERIVED in second_ingredient.restrictions
+    assert Restriction.ANIMAL_MEAT in second_ingredient.restrictions
+
+    # o método mágico __repr__ funcione como esperado
+
+    assert repr(first_ingredient) == "Ingredient('caldo de carne')"
+    assert repr(second_ingredient) == "Ingredient('bacon')"
+
+    # o método mágico __eq__ funcione como esperado;
+
+    assert second_ingredient == fourth_ingredient
+    assert first_ingredient != third_ingredient
+
+    # o método mágico __hash__ funcione como esperado
+
+    assert hash(second_ingredient) == hash(fourth_ingredient)
+    assert hash(first_ingredient) != hash(third_ingredient)
+
+    # validando o nome errado
+
+    assert first_ingredient.name == "caldo de carne"
+    assert second_ingredient.name == "bacon"
+    assert second_ingredient.name != "manteiga"
